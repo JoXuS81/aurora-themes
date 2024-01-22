ba921fa6d53ec019cc75a5c54189c263fdffb517
Jovan Stanisic <jst2016@mail.com>
2024-01-22 13:05:44 +0100

    Add all my files

---
+++ JS/header.js
@@ -0,0 +1,31 @@
+const hamburgerMenu = document.querySelector(".lines");
+const menuInside = document.querySelector(".header-content-menuInside");
+const menuOptionHome = document.querySelector(".header-content-menuInside-container-options-home");
+const menuOptionWork = document.querySelector(".header-content-menuInside-container-options-work");
+
+
+hamburgerMenu.addEventListener("click", () => 
+{
+    hamburgerMenu.classList.toggle("lines");
+    hamburgerMenu.classList.toggle("changeLines");
+    menuInside.classList.toggle("header-content-menuInside");
+    menuInside.classList.toggle("showMenu");
+});
+
+
+menuOptionHome.addEventListener("click", () => 
+{
+    hamburgerMenu.classList.remove("changeLines");
+    hamburgerMenu.classList.add("lines");
+    menuInside.classList.remove("showMenu");
+    menuInside.classList.add("header-content-menuInside");
+});
+
+
+menuOptionWork.addEventListener("click", () => 
+{
+    hamburgerMenu.classList.remove("changeLines");
+    hamburgerMenu.classList.add("lines");
+    menuInside.classList.remove("showMenu");
+    menuInside.classList.add("header-content-menuInside");
+});
